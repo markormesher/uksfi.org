7ba7c8b8f7eec16ed37762358475ca6fc27f433d
Mark Ormesher <me@markormesher.co.uk>
2014-11-01 17:19:41 +0000

    Started adding notification for interest expressed

--- db/alert-functions.php
+++ db/alert-functions.php
@@ -21,6 +21,7 @@ function notifyListingPosted($listingId) {
  * Send a listing notification to one person
  */
 function notifyUserForListing($listingId, $userId) {
+	$listing = db_getListing($listingId);
 	$profile = db_getUserProfile($userId);
 	$notifications = db_getUserPref($userId, 'r_notify_via');
 	foreach ($notifications as $n) {
@@ -36,8 +37,40 @@ function notifyUserForListing($listingId, $userId) {
 			case 'phone_1';
 			case 'phone_2':
 				$number = $profile[$n];
+				$message = 'A listing was recently posted that matches your interests: ' . $listing['title'] . '. More info: http://uksfi.org/m/36247';
 				// TODO Twilio
 				break;
 		}
 	}
+}
+
+/**
+ * Interest was expressed: find the donor and notify them
+ */
+function notifyInterestExpressed($donorId, $receiverId, $listingId) {
+	$donorProfile = db_getUserProfile($donorId);
+	$receiverProfile = db_getUserProfile($receiverId);
+	$listing = db_getListing($listingId);
+	$notifications = db_getUserPref($donorId, 'r_notify_via');
+	foreach ($notifications as $n) {
+		switch ($n) {
+			case 'yo':
+				$yoUsername = $donorProfile['yo_username'];
+				$ch = curl_init('http://api.justyo.co/yo');
+				curl_setopt($ch, CURLOPT_POST, 3);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, 'api_token=6421a408-0365-4aef-968c-09c30908db58&username=' . $yoUsername . '&link=http://www.uksfi.org');
+				curl_exec($ch);
+				break;
+
+			case 'phone_1';
+			case 'phone_2':
+				$number = $donorProfile[$n];
+				$rName = $receiverProfile['name'];
+				$rCompany = $receiverProfile['company_name'];
+				$message = $rName . (strlen($rCompany) > 0 ? ' from ' . $rCompany : '') . ' has expressed an interest in your listing, \'' . $listing['title'] . '\'. Check http://uksfi.org/m/25473 to respond.';
+				// TODO Twilio
+				break;
+		}
+	}
+
 }
